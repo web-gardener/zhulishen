65a9731c4ebebbade347b31c20ead2a55c8d842f
web-gardener <zhulishen0331@gmail.com>
2023-04-03 03:36:11 +0300

    portfolio v1

---
+++ assets/js/steller.js
@@ -0,0 +1,38 @@
+/*!
+=========================================================
+* Steller Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+	
+});
+
+setTimeout(function(){
+	$('#pre-loading').fadeOut(1000);
+	window.scrollTo(0, 0); // Move the scroll to the top of the page
+}, 5000);

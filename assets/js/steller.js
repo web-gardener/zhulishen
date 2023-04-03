6d50a82cc16bc42192a02f09eccf534591e14a51
web-gardener <zhulishen0331@gmail.com>
2023-04-03 03:52:47 +0300

    merge css and js files

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -36,3 +36,24 @@ setTimeout(function(){
 	$('#pre-loading').fadeOut(1000);
 	window.scrollTo(0, 0); // Move the scroll to the top of the page
 }, 5000);
+
+(function (document) {
+    const markers = [...document.querySelectorAll('mark')];
+    
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.style.animationPlayState = 'running';
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.8
+    });
+    
+    markers.forEach(mark => {
+      observer.observe(mark);
+    });
+  })(document);
+
+  
